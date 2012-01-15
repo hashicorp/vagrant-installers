119937b7f7d87d1870c421ab4813f441f09c3395
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-01-14 22:05:35 -0800

    mac: postinstall is actually postflight/postupgrade as well

---
+++ support/mac/postinstall.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo here > /tmp/foo
+
+# Create the symlink so that `vagrant` is available on the
+# PATH.
+ln -Fs $2/bin/vagrant /usr/bin/vagrant
+
+# Exit with a success code
+exit 0
--- support/postinstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo $2 > /tmp/foo
-ln -Fs $2/bin/vagrant /usr/bin/vagrant
-exit 0
