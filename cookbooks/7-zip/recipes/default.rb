64797bad59e4346e57159a9023ceb3ba319b90b9
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-01-16 14:31:16 -0800

    Install 7-zip

--- cookbooks/7-zip/recipes/default.rb
+++ cookbooks/7-zip/recipes/default.rb
@@ -3,6 +3,7 @@ windows_package node[:sevenzip][:package_name] do
   source node[:sevenzip][:url]
   checksum node[:sevenzip][:checksum]
   options "INSTALLDIR=\"#{node[:sevenzip][:home]}\""
+  action :install
 end
 
 # Update the PATH to add 7-zip
