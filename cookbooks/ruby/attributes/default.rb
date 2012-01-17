74d38750db3e1807d58829556172ee2d2a1f5f03
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-01-16 19:13:11 -0800

    Ruby 1.9.3 extracted

---
+++ cookbooks/ruby/attributes/default.rb
@@ -0,0 +1,4 @@
+default[:ruby][:win_url] = "http://rubyforge.org/frs/download.php/75466/ruby-1.9.3-p0-i386-mingw32.7z"
+default[:ruby][:win_checksum] = "b7b9867e7de60dbc0b8dd2151ec5bca82297d38c094a2e56c61a8d34bb859972"
+default[:ruby][:win_devkit_url] = "http://github.com/downloads/oneclick/rubyinstaller/DevKit-tdm-32-4.5.2-20110712-1620-sfx.exe"
+default[:ruby][:win_devkit_checksum] = "6230d9e552e69823b83d6f81a5dadc06958d7a17e10c3f8e525fcc61b300b2ef"
--- cookbooks/ruby/recipes/windows.rb
+++ cookbooks/ruby/recipes/windows.rb
@@ -1,1 +1,22 @@
 include_recipe "7-zip"
+
+# Download Ruby 7zip Archive (Thanks to RubyInstaller!)
+file_cache_path = ::File.expand_path(Chef::Config[:file_cache_path])
+file_cache_path = file_cache_path.gsub("/", "\\")
+ruby_filename = ::File.basename(node[:ruby][:win_url])
+ruby_download_path = ::File.join(file_cache_path, ruby_filename)
+
+remote_file ruby_download_path do
+  source   node[:ruby][:win_url]
+  checksum node[:ruby][:win_checksum]
+end
+
+# Unzip the archive and move it to the embedded directory
+unzip_directory = ::File.basename(ruby_filename, ".7z")
+unzip_directory = "#{file_cache_path}\\#{unzip_directory}"
+windows_batch "unzip-ruby" do
+  code <<-EOH
+  "#{node[:sevenzip][:home]}\\7z.exe" x #{ruby_download_path} -o#{Chef::Config[:file_cache_path]} -r -y
+  xcopy #{unzip_directory} \"#{node[:installer][:staging_dir]}\" /e /y
+  EOH
+end
