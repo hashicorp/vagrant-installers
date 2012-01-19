39d4948d020eb431c7fdb4f2b056ea77cf021051
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-01-18 23:50:49 -0800

    Use the system temp-dir for the file cache.
    Since I'm using Vagrant to build VMs for linux installers and
    VirtualBox SF don't support symlinks, I can't depend on the "cache"
    dir in the source.

--- config/solo.rb
+++ config/solo.rb
@@ -1,6 +1,7 @@
 require 'pathname'
+require 'tmpdir'
 root_dir = Pathname.new(File.expand_path("../../", __FILE__))
 
 cookbook_path root_dir.join("cookbooks").to_s
-file_cache_path root_dir.join("cache").to_s
+file_cache_path Pathname.new(Dir.tmpdir).join("chef-cache").to_s
 log_level :info
