e1e554592a5971272f4a1b5c44533ad19d06921b
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-01-19 00:01:24 -0800

    Linux: Compile libyaml

--- cookbooks/installer/recipes/ubuntu.rb
+++ cookbooks/installer/recipes/ubuntu.rb
@@ -1,1 +1,2 @@
+include_recipe "libyaml"
 include_recipe "ruby"
--- cookbooks/libyaml/recipes/default.rb
+++ cookbooks/libyaml/recipes/default.rb
@@ -1,7 +1,14 @@
-env_vars = {
-  "CFLAGS" => "-arch i386 -arch x86_64",
-  "LDFLAGS" => "-arch i386 -arch x86_64 -Wl,-install_name,@rpath/libyaml.dylib"
-}
+env_vars = {}
+
+if platform?("mac_os_x")
+  # Build a universal binary
+  env_vars["CFLAGS"] = "-arch i386 -arch x86_64"
+  env_vars["LDFLAGS"] = "-arch i386 -arch x86_64"
+
+  # Set the install name to use the @rpath so this library
+  # is relocatable
+  env_vars["LDFLAGS"] += " -Wl,-install_name,@rpath/libyaml.dylib"
+end
 
 util_autotools "libyaml" do
   file "yaml-0.1.4.tar.gz"
