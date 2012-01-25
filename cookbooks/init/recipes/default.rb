8d43597857f1bfbbba76307253138e86a2f83cc5
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-01-24 22:23:56 -0800

    Fix issue where file cache isn't ready in time

--- cookbooks/init/recipes/default.rb
+++ cookbooks/init/recipes/default.rb
@@ -1,8 +1,11 @@
-# Make sure the file cache exists.
+# Make sure the file cache exists. We do this during the compilation
+# phase since the git checkout (which also happens during the compilation
+# phase) depends on this.
 directory Chef::Config[:file_cache_path] do
   mode 0755
   recursive true
-end
+  action :nothing
+end.run_action(:create)
 
 # Create the directories which will store our staging environment
 # that we use to package the installer.
