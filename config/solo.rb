5892541a49425346f31037a41df8e59cd868d6cc
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-01-26 22:22:42 -0800

    Make cache in our directory again

--- config/solo.rb
+++ config/solo.rb
@@ -3,5 +3,5 @@ require 'tmpdir'
 root_dir = Pathname.new(File.expand_path("../../", __FILE__))
 
 cookbook_path root_dir.join("cookbooks").to_s
-file_cache_path root_dir.join("chef-cache").to_s
+file_cache_path root_dir.join("cache").to_s
 log_level :info
