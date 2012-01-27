d0087938388c9199919d38bdd398c44c6ae4091f
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-01-26 22:22:21 -0800

    Arch: Build binary package

--- cookbooks/package/recipes/arch.rb
+++ cookbooks/package/recipes/arch.rb
@@ -3,7 +3,13 @@
 # This is the directory where we'll put all the intermediary source
 # files that are needed to build a Arch binary package.
 setup_dir = ::File.join(Chef::Config[:file_cache_path], "arch_setup")
-source_name = "#{vars[:pkgname]}-#{vars[:pkgver]}.tar.gz"
+
+vars = {
+  :pkgname => "vagrant",
+  :pkgver  => node[:vagrant][:version],
+  :arch    => node[:kernel][:machine],
+  :source  => "vagrant-#{node[:vagrant][:version]}.tar.gz"
+}
 
 directory setup_dir do
   mode 0755
@@ -12,28 +18,19 @@ end
 
 # Tar up the staging directory
 execute "tar-staging-dir" do
-  command "tar cvzf #{source_name} #{staging_dir}"
-  pwd     setup_dir
+  command "tar cvzf #{vars[:source]} #{staging_dir}"
+  cwd     setup_dir
 end
 
 # The PKGBUILD is used to create the binary package
-vars = {
-  :pkgname => "vagrant",
-  :pkgver  => node[:vagrant][:version],
-  :arch    => node[:kernel][:machine],
-  :source  => source_name
-}
-
 template ::File.join(setup_dir, "PKGBUILD") do
-  source "PKGBUILD.erb"
+  source "arch/PKGBUILD.erb"
   variables vars
 end
-=begin
+
 # Make the package
 execute "makepkg" do
   command "makepkg --asroot"
   cwd     setup_dir
 end
 
-# TODO: Read the pkg.tar.gz file and copy into the dist dir
-=end
