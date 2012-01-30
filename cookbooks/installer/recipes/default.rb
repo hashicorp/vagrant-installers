fb15cb2b727d262c1cac887be8e939f9f5b64cd8
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-01-29 23:28:07 -0800

    Install Chef can only run one process at a time

--- cookbooks/installer/recipes/default.rb
+++ cookbooks/installer/recipes/default.rb
@@ -1,5 +1,46 @@
-include_recipe "init"
+#----------------------------------------------------------------------
+# Setup the directories
+#----------------------------------------------------------------------
+# Make sure the file cache exists. We do this during the compilation
+# phase since the git checkout (which also happens during the compilation
+# phase) depends on this.
+directory Chef::Config[:file_cache_path] do
+  mode 0755
+  recursive true
+  action :nothing
+end.run_action(:create)
 
+# Create the directories which will store our staging environment
+# that we use to package the installer.
+["", "bin", "embedded"].each do |subdir|
+  directory ::File.join(node[:installer][:staging_dir], subdir) do
+    mode  0755
+    recursive true
+  end
+end
+
+#----------------------------------------------------------------------
+# Obtain File Lock
+#----------------------------------------------------------------------
+# We do this so that only one build can run at any given time.
+$_chef_lock_file = File.open(File.join(Chef::Config[:file_cache_path], "chef_lock"), "w+")
+if $_chef_lock_file.flock(File::LOCK_EX | File::LOCK_NB) === false
+  $_chef_lock_file = nil
+  Chef::Log.info("Another Chef process running. Not running.")
+  exit(true)
+end
+
+# Unlock the file at exit, just to be sure
+at_exit do
+  if $_chef_lock_file
+    $_chef_lock_file.flock(File::LOCK_UN)
+    $_chef_lock_file = nil
+  end
+end
+
+#----------------------------------------------------------------------
+# Platform-specific stuff
+#----------------------------------------------------------------------
 if platform?("mac_os_x")
   include_recipe "installer::mac"
 elsif platform?("windows")
