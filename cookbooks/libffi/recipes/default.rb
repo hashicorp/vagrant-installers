90cfce197070656c5b68fec950949173000b238c
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-02-08 00:46:48 -0800

    libffi: Move headers to a standard location

--- cookbooks/libffi/recipes/default.rb
+++ cookbooks/libffi/recipes/default.rb
@@ -1,3 +1,11 @@
+# Compile libffi
 util_autotools "libffi" do
   file "libffi-3.0.10.tar.gz"
+end
+
+# Move libffi headers. libffi installs its headers in a
+# really strange place, so we move them into the standard
+# location.
+execute "libffi-headers-move" do
+  command "mv #{embedded_dir}/lib/libffi-*/include/* #{embedded_dir}/include"
 end
