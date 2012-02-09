707c95425e7597d355ef51a86f300a21d95f8dfe
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-02-08 22:11:50 -0800

    CentOS: Install OpenSSL and such

--- cookbooks/installer/recipes/centos.rb
+++ cookbooks/installer/recipes/centos.rb
@@ -1,4 +1,11 @@
+# Some dependencies for Ruby extensions we want installed
+include_recipe "libffi"
 include_recipe "libyaml"
+include_recipe "zlib"
+include_recipe "openssl"
+include_recipe "readline"
+
+# The actual dependencies of Vagrant
 include_recipe "ruby"
 include_recipe "vagrant"
 include_recipe "package"
