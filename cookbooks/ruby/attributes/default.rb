ca190abf48411024fe53cc40bd3f506eae30da51
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-03-07 22:21:00 -0800

    Windows should use Ruby 1.9.3p125 and latest DevKit

--- cookbooks/ruby/attributes/default.rb
+++ cookbooks/ruby/attributes/default.rb
@@ -1,4 +1,4 @@
-default[:ruby][:win_url] = "http://rubyforge.org/frs/download.php/75466/ruby-1.9.3-p0-i386-mingw32.7z"
-default[:ruby][:win_checksum] = "b7b9867e7de60dbc0b8dd2151ec5bca82297d38c094a2e56c61a8d34bb859972"
-default[:ruby][:win_devkit_url] = "http://github.com/downloads/oneclick/rubyinstaller/DevKit-tdm-32-4.5.2-20110712-1620-sfx.exe"
-default[:ruby][:win_devkit_checksum] = "6230d9e552e69823b83d6f81a5dadc06958d7a17e10c3f8e525fcc61b300b2ef"
+default[:ruby][:win_url] = "http://rubyforge.org/frs/download.php/75849/ruby-1.9.3-p125-i386-mingw32.7z"
+default[:ruby][:win_checksum] = "6b7540775d99422174ff97303843d1409a08e623909553e1c727ec9b77f8faa6"
+default[:ruby][:win_devkit_url] = "https://github.com/downloads/oneclick/rubyinstaller/DevKit-tdm-32-4.5.2-20111229-1559-sfx.exe"
+default[:ruby][:win_devkit_checksum] = "6c3af5487dafda56808baf76edd262b2020b1b25ab86aabf972629f4a6a54491"
