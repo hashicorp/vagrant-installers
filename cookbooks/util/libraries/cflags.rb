b1134a9b639fe2e3ff271dd056c898907ecb3dad
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-06-13 21:00:38 -0700

    Mac: Support down to Mac OS X 10.5

--- cookbooks/util/libraries/cflags.rb
+++ cookbooks/util/libraries/cflags.rb
@@ -2,7 +2,7 @@
 def cflags
   flags = {
     "LDFLAGS" => "-I#{embedded_dir}/include -L#{embedded_dir}/lib",
-    "CLFAGS"  => "-I#{embedded_dir}/include -L#{embedded_dir}/lib"
+    "CFLAGS"  => "-I#{embedded_dir}/include -L#{embedded_dir}/lib"
   }
 
   if platform?("windows")
@@ -13,6 +13,10 @@ def cflags
   elsif platform?("mac_os_x")
     # Actually not quite sure if I need this... yet
     flags["LDFLAGS"] += " -R#{embedded_dir}/lib"
+
+    # Build down to Mac OS X 10.5
+    flags["LDFLAGS"] += " -mmacosx-version-min=10.5"
+    flags["CFLAGS"] += " -mmacosx-version-min=10.5"
   end
 
   return flags
