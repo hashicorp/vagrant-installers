25e33c952b52689cf9c0c619b4b3baca6a9c0ae2
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-12-01 16:04:12 -0800

    Remove unused files

--- Gemfile
+++
@@ -1,3 +0,0 @@
-source "http://rubygems.org"
-
-gem "fog", "~> 1.1.2"
--- config/solo.rb
+++
@@ -1,7 +0,0 @@
-require 'pathname'
-require 'tmpdir'
-root_dir = Pathname.new(File.expand_path("../../", __FILE__))
-
-cookbook_path root_dir.join("cookbooks").to_s
-file_cache_path root_dir.join("cache").to_s
-log_level :info
