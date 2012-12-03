6403ffd5c1c353dcd6c9cee0e838f063bde0c2e7
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-12-02 22:39:25 -0800

    run.sh takes 3 params: revision version output-dir

--- run.sh
+++ run.sh
@@ -3,6 +3,12 @@
 # This script will actually run the puppet code here.
 TMP_CONFIG_DIR=/tmp/puppet_config
 
+# Verify arguments
+if [ "$#" -ne "3" ]; then
+  echo "Usage: $0 revision version output_directory" >&2
+  exit 1
+fi
+
 # We need to create a temporary configuration directory because Puppet
 # needs to be able to set the permissions on this and if we call this
 # from a filesystem that doesn't support that (VMWare shared folders),
@@ -19,9 +25,10 @@ if [ `uname` = 'Darwin' ]; then
   sudo dscl . -create /groups/puppet passwd '*'
 fi
 
-# XXX: Temporary
-export FACTER_param_vagrant_revision="d84b71d73eefb9ea554288c92f64020e719e1135"
-export FACTER_param_vagrant_version="1.1.0"
+# Export the parameters for Puppet
+export FACTER_param_vagrant_revision="$1"
+export FACTER_param_vagrant_version="$2"
+export FACTER_param_dist_dir="$3"
 
 # Invoke Puppet
 sudo -E puppet apply \
