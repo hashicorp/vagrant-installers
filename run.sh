ddbc8f383345aca4cdd0ab2814d5a88b80941bc3
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-12-10 13:38:11 -0800

    Use a random temporary directory, don't require sudo

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #
 # This script will actually run the puppet code here.
-TMP_CONFIG_DIR=/tmp/puppet_config
 
 # Verify arguments
 if [ "$#" -ne "3" ]; then
@@ -18,9 +17,8 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 # needs to be able to set the permissions on this and if we call this
 # from a filesystem that doesn't support that (VMWare shared folders),
 # then Puppet will fail.
-sudo rm -rf ${TMP_CONFIG_DIR}
-sudo mkdir -p ${TMP_CONFIG_DIR}
-sudo cp -R ${DIR}/config/* ${TMP_CONFIG_DIR}
+TMP_CONFIG_DIR=$(mktemp -d vagrant-installer.XXXXXX)
+cp -R ${DIR}/config/* ${TMP_CONFIG_DIR}
 
 # Export the parameters for Puppet
 export FACTER_param_vagrant_revision="$1"
@@ -29,7 +27,7 @@ export FACTER_param_dist_dir="$3"
 
 # Invoke Puppet
 cd $DIR
-sudo -E puppet apply \
+puppet apply \
   --confdir=${TMP_CONFIG_DIR} \
   --modulepath=${DIR}/modules \
   ${DIR}/manifests/init.pp
