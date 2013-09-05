99fa26994c1afbb6f538d4e90d6f65194bf8dd74
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-09-05 10:03:00 -0700

    vagrant_installer: downcase the archive name

--- modules/vagrant_installer/manifests/staging.pp
+++ modules/vagrant_installer/manifests/staging.pp
@@ -8,7 +8,8 @@ class vagrant_installer::staging {
   $staging_dir     = $vagrant_installer::params::staging_dir
   $vagrant_version = $vagrant_installer::params::vagrant_version
 
-  $archive_name = "vagrant_${vagrant_version}_${kernel}_${hardwaremodel}"
+  $archive_name_raw = "vagrant_${vagrant_version}_${kernel}_${hardwaremodel}"
+  $archive_name = inline_template("<%= archive_name_raw.downcase %>")
   $archive_path = "${dist_dir}${file_sep}${archive_name}.zip"
 
   case $kernel {
