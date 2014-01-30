b8faa69173f11bb230269584d6ae36721b4050dd
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-01-29 16:01:54 -0800

    pacakge: scripts for Ubuntu

---
+++ package/package.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+#
+# Builds the package for Vagrant unix-like systems (Mac OS X and Linux).
+set -e
+
+# Verify arguments
+if [ "$#" -ne "3" ]; then
+  echo "Usage: $0 SUBSTRATE-PATH VAGRANT-REVISION VAGRANT-VERSION" >&2
+  exit 1
+fi
+
+SUBSTRATE_PATH=$1
+VAGRANT_REVISION=$2
+VAGRANT_VERSION=$3
+
+# Get our directory
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+#--------------------------------------------------------------------
+# Common Operations
+#--------------------------------------------------------------------
+# Copy the substrate and unzip it
+SUBSTRATE_TMP_DIR=$(mktemp -d tmp.XXXXXXXXXX)
+cp $SUBSTRATE_PATH ${SUBSTRATE_TMP_DIR}/substrate.zip
+pushd $SUBSTRATE_TMP_DIR
+unzip substrate.zip
+popd
+SUBSTRATE_DIR=$(cd ${SUBSTRATE_TMP_DIR}/substrate; pwd)
+
+# Install Vagrant
+${DIR}/support/install_vagrant.sh ${SUBSTRATE_DIR} ${VAGRANT_REVISION}
+
+# Create a writeable temporary directory
+TMP_DIR=$(mktemp -d tmp.XXXXXXXXXX)
+TMP_DIR=$(cd ${TMP_DIR}; pwd)
+export TMPDIR="${TMP_DIR}"
+
+#--------------------------------------------------------------------
+# OS-specific packaging
+#--------------------------------------------------------------------
+# Debian
+if [ -f "/etc/debian_version" ]; then
+    ${DIR}/support/package_ubuntu.sh ${SUBSTRATE_DIR} ${VAGRANT_VERSION}
+fi
+
+# Clean up the temporary dir
+rm -rf ${SUBSTRATE_TMP_DIR} ${TMP_DIR}
---
+++ package/support/package_ubuntu.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+set -e
+
+# Verify arguments
+if [ "$#" -ne "2" ]; then
+  echo "Usage: $0 SUBSTRATE-DIR VAGRANT-VERSION" >&2
+  exit 1
+fi
+
+SUBSTRATE_DIR=$1
+VAGRANT_VERSION=$2
+OUTPUT_PATH="`pwd`/vagrant_${VAGRANT_VERSION}.deb"
+
+# Work in a temporary directory
+rm -rf package-staging
+mkdir -p package-staging
+STAGING_DIR=$(cd package-staging; pwd)
+pushd $STAGING_DIR
+
+# Make some directories
+mkdir -p ./usr/bin
+mkdir -p ./opt/vagrant
+mv ${SUBSTRATE_DIR}/bin ./opt/vagrant
+mv ${SUBSTRATE_DIR}/embedded ./opt/vagrant
+
+# Create the Linux script proxy
+cat <<EOF >./usr/bin/vagrant
+#!/usr/bin/env bash
+#
+# This script just forwards all arguments to the real vagrant binary.
+
+/opt/vagrant/bin/vagrant "$@"
+EOF
+chmod +x ./usr/bin/vagrant
+
+# Package it up!
+fpm -p ${OUTPUT_PATH} \
+  -n vagrant \
+  -v $VAGRANT_VERSION \
+  -s dir \
+  -t deb \
+  --prefix '/' \
+  --maintainer "HashiCorp <hello@hashicorp.com>" \
+  --epoch 1 \
+  --deb-user root \
+  --deb-group root \
+  .
+
+# Exit the directory and clean it
+popd
+rm -rf $STAGING_DIR
