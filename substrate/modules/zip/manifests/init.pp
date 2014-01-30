6416a774d5df953a12de4025d1d5ba23918d2b8d
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-01-29 18:16:01 -0800

    substrate: fix some problems on older versions of CentOS

--- substrate/modules/zip/manifests/init.pp
+++ substrate/modules/zip/manifests/init.pp
@@ -1,5 +1,11 @@
 class zip {
   case $operatingsystem {
+    'CentOS': {
+      package { "zip":
+        ensure => installed,
+      }
+    }
+
     'Ubuntu': {
       package { ["zip", "unzip"]:
         ensure => installed,
