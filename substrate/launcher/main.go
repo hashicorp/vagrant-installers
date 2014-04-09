13294e505db125f40895e49c0895e6489d8caf84
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-04-09 09:14:35 -0700

    Update launcher.exe to return proper exit code

--- substrate/launcher/main.go
+++ substrate/launcher/main.go
@@ -6,6 +6,7 @@ import (
 	"os/exec"
 	"path/filepath"
 	"runtime"
+	"syscall"
 
 	"github.com/mitchellh/osext"
 )
@@ -107,5 +108,21 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Exec error: %s\n", err)
 		os.Exit(1)
 	}
-	cmd.Wait()
+
+	exitCode := 0
+	if err := cmd.Wait(); err != nil {
+		if exiterr, ok := err.(*exec.ExitError); ok {
+			// The program has exited with an exit code != 0
+
+			// This works on both Unix and Windows. Although package
+			// syscall is generally platform dependent, WaitStatus is
+			// defined for both Unix and Windows and in both cases has
+			// an ExitStatus() method with the same signature.
+			if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
+				exitCode = status.ExitStatus()
+			}
+		}
+	}
+
+	os.Exit(exitCode)
 }
