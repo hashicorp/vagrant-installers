253fc77efd033f0a430398099d77a231397e87bc
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-12-02 09:31:57 -0800

    Fix source repo calculation

--- package/support/install_vagrant.sh
+++ package/support/install_vagrant.sh
@@ -28,7 +28,7 @@ pushd $TMP_DIR
 
 # Download Vagrant and extract
 SOURCE_REPO=${VAGRANT_REPO:-mitchellh/vagrant}
-SOURCE_PREFIX=${VAGRANT_REPO/\//-}
+SOURCE_PREFIX=${SOURCE_REPO/\//-}
 SOURCE_URL="https://api.github.com/repos/${SOURCE_REPO}/tarball/${VAGRANT_REV}"
 if [ -z "${VAGRANT_TOKEN}" ]; then
     curl -L ${SOURCE_URL} > vagrant.tar.gz
