547e3615280a17352a1caa5902e07f314103f874
Chris Roberts <code@chrisroberts.org>
2017-04-12 10:42:13 -0700

    Force full release version into URL

--- package/vagrant-scripts/centos.sh
+++ package/vagrant-scripts/centos.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+relver=$(awk '{print $3}' /etc/redhat-release)
+
 # use vault to access old packages
 sed -i 's/mirror.centos.org\/centos/vault.centos.org/g' /etc/yum.repos.d/CentOS-Base.repo
+sed -i "s/\$releasever/${relver}/g" /etc/yum.repos.d/CentOS-Base.repo
 sed -i 's/#baseurl/baseurl/g' /etc/yum.repos.d/CentOS-Base.repo
 sed -i 's/mirrorlist=.*$//g' /etc/yum.repos.d/CentOS-Base.repo
 
