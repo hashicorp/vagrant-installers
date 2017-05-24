37979e4477939fc2e54dcead76507a6a27a0f543
Chris Roberts <code@chrisroberts.org>
2017-05-24 13:22:57 -0700

    Reboot last and set ssh to start when system is up

--- packer/vagrant/scripts/windows/vm-guest-tools.bat
+++ packer/vagrant/scripts/windows/vm-guest-tools.bat
@@ -22,11 +22,11 @@ if not exist "C:\Windows\Temp\windows.iso" (
 )
 
 cmd /c ""C:\Program Files\7-Zip\7z.exe" x "C:\Windows\Temp\windows.iso" -oC:\Windows\Temp\VMWare"
-cmd /c C:\Windows\Temp\VMWare\setup.exe /S /v"/qn REBOOT=R\"
+start /wait C:\Windows\Temp\VMWare\setup.exe /S /v"/qn REBOOT=R\"
 
 rd /Q "C:\Windows\Temp\vmware-tools.tar"
 rd /Q "C:\Windows\Temp\windows.iso"
-rd /S /Q "C:\Windows\Temp\VMware"
+::rd /S /Q "C:\Windows\Temp\VMware"
 goto :done
 
 :virtualbox
