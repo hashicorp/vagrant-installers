37f58dcba895cf49489f08fd63bd0994811d8226
Chris Roberts <code@chrisroberts.org>
2017-06-01 14:46:52 -0700

    Remove tool installation from substrate provision on windows

--- substrate/vagrant-scripts/win.ps1
+++ substrate/vagrant-scripts/win.ps1
@@ -21,18 +21,7 @@ function savepowershellfromitself {
     $field2.SetValue( $consoleHost, [Console]::Out )
 }
 
-savepowershellfromitself
-
-(New-Object System.Net.WebClient).DownloadFile('https://github.com/git-for-windows/git/releases/download/v2.13.0.windows.1/Git-2.13.0-64-bit.exe', 'C:\Windows\Temp\git.exe')
-
-$result = Start-Process "C:\Windows\Temp\git.exe" "/quiet" -Wait
-if($result.ExitCode -ne 0){
-  Write-Error "Failed to install Git"
-  Exit 1
-}
 Write-Host "Starting substrate build"
-mkdir C:\vagrant\substrate-assets
-
-savepowershellfromitself
+[System.IO.Directory]::CreateDirectory("C:\vagrant\substrate-assets") | Out-Null
 
 Invoke-Expression "C:\vagrant\substrate\run.ps1 -OutputDir C:\vagrant\substrate-assets"
