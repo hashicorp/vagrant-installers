4f8f4c73748fbc2e38a841e68310861b582902ea
Chris Roberts <code@chrisroberts.org>
2017-06-09 14:12:54 -0700

    Remove launcher build from substrate run scripts

--- substrate/run.ps1
+++ substrate/run.ps1
@@ -12,23 +12,6 @@ Param(
 
 # Get the directory to this script
 $Dir = Split-Path $script:MyInvocation.MyCommand.Path
-
-# Build launcher
-Write-Host "Building golang launcher"
-$GoBin = "C:\Go\bin\go.exe"
-$LauncherDir = [System.IO.Path]::Combine($Dir, "launcher")
-Set-Location $LauncherDir
-$result = Start-Process $GoBin "get github.com/mitchellh/osext" -Wait
-if($result.ExitCode -ne 0){
-  Write-Error "Failed to install osext dependency"
-  Exit 1
-}
-$result = Start-Process $GoBin "-o ..\modules\vagrant_substrate\files\launcher main.go" -Wait
-if($result.ExitCode -ne 0){
-  Write-Error "Failed to build launcher"
-  Exit 1
-}
-Write-Host "Completed launcher build"
 Set-Location $Dir
 
 # We need to create a temporary configuration directory because Puppet
