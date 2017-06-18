5375decd1cb0ebb3a096141bc9754c1c3db1b9bc
Chris Roberts <code@chrisroberts.org>
2017-06-18 09:40:49 -0700

    Remove package provisioning setup

--- package/vagrant-scripts/centos.sh
+++ package/vagrant-scripts/centos.sh
@@ -1,18 +1,6 @@
 #!/bin/sh
 
-yum install -y epel-release
-yum install -y python-hashlib git
-
 yum install -y nc zip unzip
-
-REPO_RPM_URL="http://yum.puppetlabs.com/puppetlabs-release-el-5.noarch.rpm"
-rm -f /tmp/puppet.rpm
-curl -o /tmp/puppet.rpm -L $REPO_RPM_URL
-rpm -i /tmp/puppet.rpm
-yum install -y puppet ruby-devel
-
-gem install json -v '~> 1.8.6' --no-ri --no-rdoc
-gem install fpm -v '~> 0.4.0' --no-ri --no-rdoc
 
 # if the proxy is around, use it
 nc -z -w3 192.168.1.1 8123 && export http_proxy="http://192.168.1.1:8123"
