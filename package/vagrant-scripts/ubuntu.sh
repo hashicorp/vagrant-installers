eab726ca4e7fe7c07622dc90cc9e5236efd311aa
Chris Roberts <code@chrisroberts.org>
2017-06-19 10:04:07 -0700

    Update path to make fpm available

--- package/vagrant-scripts/ubuntu.sh
+++ package/vagrant-scripts/ubuntu.sh
@@ -4,6 +4,11 @@
 nc -z -w3 192.168.1.1 8123 && export http_proxy="http://192.168.1.1:8123"
 
 chmod 755 /vagrant/package/package.sh
+apt-get update -yq
+apt-get install -yq build-essential
+
+# Ensure we can get to fpm
+export PATH=$PATH:/var/lib/gems/1.8/bin
 
 /vagrant/package/package.sh /vagrant/substrate-assets/substrate_ubuntu_$(uname -m).zip master
 mkdir -p /vagrant/pkg
