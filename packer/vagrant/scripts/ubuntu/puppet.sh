bd9a1161446f9d3428d980063a4448d26cbb8d7a
Chris Roberts <croberts@hashicorp.com>
2018-01-06 09:01:35 -0800

    Update puppet post-install to make the old version usable

--- packer/vagrant/scripts/ubuntu/puppet.sh
+++ packer/vagrant/scripts/ubuntu/puppet.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-gem install puppet -v '~> 3.0' --no-document
+
+gem install puppet -v 3.8.7 --no-document
+
+# install missing gem dependency
+gem install xmlrpc --no-document
+
+# Puppet assumes Syck will be in use, but it is not. So kill
+# the monkey patch so nothing is modified and puppet will
+# load successfully
+echo "" > /var/lib/gems/2.4.0/gems/puppet-3.8.7/lib/puppet/vendor/safe_yaml/lib/safe_yaml/syck_node_monkeypatch.rb
