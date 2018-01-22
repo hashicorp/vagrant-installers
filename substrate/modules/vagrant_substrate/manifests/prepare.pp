a8f57fdcade9987e32d9e29e72ee8f1d75e620e4
Chris Roberts <croberts@hashicorp.com>
2018-01-22 15:10:19 -0800

    Do not create the etc directories within embedded

--- substrate/modules/vagrant_substrate/manifests/prepare.pp
+++ substrate/modules/vagrant_substrate/manifests/prepare.pp
@@ -39,8 +39,8 @@ class vagrant_substrate::prepare {
       $cache_dir,
       "${staging_dir_32}/bin",
       "${staging_dir_64}/bin",
-      "${embedded_dir_32}/etc",
-      "${embedded_dir_64}/etc",
+      "${embedded_dir_32}/bin",
+      "${embedded_dir_64}/bin",
       $output_dir,
     ]
   } else {
