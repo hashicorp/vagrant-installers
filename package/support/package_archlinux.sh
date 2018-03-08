97334be516bfa3256c94540bd9c0e87db3ba4103
Chris Roberts <croberts@hashicorp.com>
2018-03-08 09:55:46 -0800

    Fix version handling within arch helpers

--- package/support/package_archlinux.sh
+++ package/support/package_archlinux.sh
@@ -15,10 +15,9 @@ TMP_DIR=$(mktemp -d tmp.XXXXXXXXX)
 pushd $TMP_DIR
 
 if [ -f "${VAGRANT_GEM_PATH}" ]; then
-    pacman -S --noconfirm ruby
     cp "${VAGRANT_GEM_PATH}" ./vagrant.gem
     gem unpack ./vagrant.gem
-    VAGRANT_VERSION=$(cat vagrant/version.txt | sed -e 's/\.[^0-9]*$//')
+    export VAGRANT_VERSION=$(cat vagrant/version.txt | sed -e 's/\.[^0-9]*$//')
     rm -rf ./vagrant
     cp "${DIR}/archlinux/PKGBUILD.local" ./PKGBUILD
     tar -f substrate.tar.gz --directory="${DIR}/../../" -cz substrate/
@@ -28,13 +27,18 @@ else
         echo "Usage: $0 VAGRANT-VERSION" >&2
         exit 1
     fi
-    VAGRANT_VERSION=$1
+    export VAGRANT_VERSION=$1
+    export CLEAN_VAGRANT_VERSION=$(echo $VAGRANT_VERSION | sed 's/^v//' | tr -d ' -')
     cp "${DIR}/archlinux/PKGBUILD" ./PKGBUILD
 fi
 
 makepkg --syncdeps --force --noconfirm
 
+if [ "${CLEAN_VAGRANT_VERSION}" != "" ]; then
+    VAGRANT_VERSION=$CLEAN_VAGRANT_VERSION
+fi
+
 mv *.xz "${ORIGIN}/vagrant_${VAGRANT_VERSION}_x86_64.tar.xz"
 
 popd
-#rm -rf "${TMP_DIR}"
+rm -rf "${TMP_DIR}"
