92cd77b61910eecb4fa6dfe958049146f982b8a7
Chris Roberts <croberts@hashicorp.com>
2018-03-14 16:30:06 -0700

    Apply updates to executable files as well

--- substrate/modules/vagrant_substrate/files/linux_rpath.sh
+++ substrate/modules/vagrant_substrate/files/linux_rpath.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -x
+
 function relpath() {
     path_to=`readlink -f "$2"`
     source=`readlink -f "$1"`
@@ -13,13 +15,23 @@ embedded_dir=$1
 for so_path in $(find "${embedded_dir}" -name "*.so"); do
     chrpath --list "${so_path}"
     if [ $? -eq 0 ]; then
-        echo "-> ${so_path}"
         set -e
         so_dir=$(dirname "${so_path}")
         rel_embedded=$(relpath "${so_dir}" "${embedded_dir}")
         rpath="\$ORIGIN/${rel_embedded}/lib:\$ORIGIN/${rel_embedded}/lib64"
         chrpath --replace "${rpath}" "${so_path}"
-        chrpath --convert "${so_path}"
+        set +e
+    fi
+done
+
+for exe_path in $(find "${embedded_dir}" -type f); do
+    chrpath --list "${exe_path}"
+    if [ $? -eq 0 ]; then
+        set -e
+        exe_dir=$(dirname "${exe_path}")
+        rel_embedded=$(relpath "${exe_dir}" "${embedded_dir}")
+        rpath="\$ORIGIN/${rel_embedded}/lib:\$ORIGIN/${rel_embedded}/lib64"
+        chrpath --replace "${rpath}" "${exe_path}"
         set +e
     fi
 done
