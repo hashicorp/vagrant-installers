284e128f925524a6a2e2ca0ab26a94b13b3f1117
Chris Roberts <croberts@hashicorp.com>
2018-04-06 14:15:01 -0700

    Add proper environment variable behavior to wrapper script

--- package/appimage/vagrant_wrapper.sh
+++ package/appimage/vagrant_wrapper.sh
@@ -6,16 +6,26 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 export VAGRANT_BIN_DIR="${DIR}"
 export VAGRANT_USR_DIR="${DIR}/../"
-export GEM_HOME="$DIR/../gembundle"
-export GEM_PATH="$DIR/../gembundle"
+export GEM_HOME="${DIR}/../gembundle"
+export GEM_PATH="${DIR}/../gembundle"
 export RUBYLIB=$( "${DIR}/ruby2.4" -e "puts $:.join(':').gsub('././', ENV['VAGRANT_USR_DIR'])")
-export SSL_CERT_FILE="$DIR/../../etc/ssl/ca-certificates.crt"
-export CURL_CA_BUNDLE="$DIR/../../etc/ssl/ca-certificates.crt"
-export LDFLAGS="-L$DIR/../lib/x86_64-linux-gnu -L$DIR/../../lib/x86_64-linux-gnu"
-export CFLAGS="-I$DIR/../include"
-export CPPFLAGS="-I$DIR/../include"
+if [ "${SSL_CERT_FILE}" = "" ]; then
+    export SSL_CERT_FILE="${DIR}/../../etc/ssl/ca-certificates.crt"
+fi
+if [ "${CURL_CA_BUNDLE}" = "" ]; then
+    export CURL_CA_BUNDLE="${DIR}/../../etc/ssl/ca-certificates.crt"
+fi
+export LDFLAGS="-L${DIR}/../lib/x86_64-linux-gnu -L${DIR}/../../lib/x86_64-linux-gnu ${LDFLAGS}"
+export CFLAGS="-I${DIR}/../include ${CFLAGS}"
+export CPPFLAGS="-I${DIR}/../include ${CPPFLAGS}"
 export VAGRANT_INSTALLER_ENV="1"
 export VAGRANT_INSTALLER_EMBEDDED_DIR="${DIR}/../.."
-export PATH="$DIR:$PATH"
+export NOKOGIRI_USE_SYSTEM_LIBRARIES="1"
 
-"$DIR/ruby2.4" -- "$DIR/../gembundle/bin/vagrant" "$@"
+if [ "${VAGRANT_PREFER_SYSTEM_BIN}" != "" -a "${VAGRANT_PREFER_SYSTEM_BIN}" != "0" ]; then
+    export PATH="${PATH}:${DIR}"
+else
+    export PATH="${DIR}:${PATH}"
+fi
+
+"${DIR}/ruby2.4" -- "${DIR}/../gembundle/bin/vagrant" "$@"
