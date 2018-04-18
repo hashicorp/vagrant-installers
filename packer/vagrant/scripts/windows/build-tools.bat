66a1e31b9deeb5819b66319ba58ecd4f7755de00
Chris Roberts <croberts@hashicorp.com>
2018-04-18 11:01:52 -0700

    Fix path to use directory

--- packer/vagrant/scripts/windows/build-tools.bat
+++ packer/vagrant/scripts/windows/build-tools.bat
@@ -4,4 +4,4 @@ powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.Security
 
 start /wait C:\Windows\Temp\build-tools.exe /Passive /AdminFile A:\buildtools-adminfile.xml
 
-setx PATH "%PATH%;C:\Program Files (x86)\Windows Kits\8.1\bin\x86\signtool.exe" /m
+setx PATH "%PATH%;C:\Program Files (x86)\Windows Kits\8.1\bin\x86\" /m
