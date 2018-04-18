7cbc7c9e0d4674e6d0eaf9dd9008734913a86c86
Chris Roberts <croberts@hashicorp.com>
2018-04-18 16:31:10 -0700

    Fixes msys2 installation by removing conflict that corrupted installation
    Also updates the msys2 related scripts to not use the `start /wait` method
    as it causes subsequent commands to begin too early.

--- packer/vagrant/scripts/windows/msys2.bat
+++ packer/vagrant/scripts/windows/msys2.bat
@@ -2,8 +2,10 @@ if not exist "C:\Windows\Temp\msys2.exe" (
 powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12; (New-Object System.Net.WebClient).DownloadFile('http://repo.msys2.org/distrib/x86_64/msys2-x86_64-20161025.exe', 'C:\Windows\Temp\msys2.exe')" <NUL
 )
 
-start /wait C:\Windows\Temp\msys2.exe --script A:\msys2-install.qs
-start /wait C:\msys64\usr\bin\bash.exe --login -c "pacman -Syu --noconfirm"
-start /wait C:\msys64\usr\bin\bash.exe --login -c "pacman -Syu --noconfirm"
-start /wait C:\msys64\usr\bin\bash.exe --login -c "pacman -S --noconfirm zip unzip"
-start /wait C:\msys64\usr\bin\bash.exe --login -c "pacman -S --noconfirm base-devel"
+C:\Windows\Temp\msys2.exe --script A:\msys2-install.qs
+
+C:\msys64\usr\bin\bash.exe --login -c "pacman -R --noconfirm catgets libcatgets"
+C:\msys64\usr\bin\bash.exe --login -c "pacman -Syu --noconfirm"
+C:\msys64\usr\bin\bash.exe --login -c "pacman -Syu --noconfirm"
+C:\msys64\usr\bin\bash.exe --login -c "pacman -S --noconfirm zip unzip"
+C:\msys64\usr\bin\bash.exe --login -c "pacman -S --noconfirm base-devel"
