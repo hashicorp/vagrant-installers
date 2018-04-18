d2c09c122005e076169befaae03ee31239d29544
Chris Roberts <croberts@hashicorp.com>
2018-04-18 16:48:50 -0700

    Replace powershell based service pack install with bat

---
+++ packer/vagrant/scripts/windows/win-7-sp1.bat
@@ -0,0 +1,8 @@
+echo "Downloading Windows 7 Service Pack 1"
+
+if not exist "C:\Windows\Temp\sp1.exe" (
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12; (New-Object System.Net.WebClient).DownloadFile('https://download.microsoft.com/download/0/A/F/0AFB5316-3062-494A-AB78-7FB0D4461357/windows6.1-KB976932-X64.exe', 'C:\Windows\Temp\sp1.exe')" <NUL
+
+echo "Installing Windows 7 Service Pack 1"
+
+start /wait C:\Windows\Temp\sp1.exe /unattend /forcerestart
