25a9bd0f811e1b5c151137fb473eff525b3c2904
Chris Roberts <croberts@hashicorp.com>
2018-08-17 13:19:53 -0700

    Ensure unzip utility is available

--- .runner-vmware.boxes.sh
+++ .runner-vmware.boxes.sh
@@ -16,6 +16,7 @@ set -e
 
 if [ "${INSTALL_PACKER}" != "" ]
 then
+    apt-get install -yq unzip
     curl -o packer.zip "https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip"
     unzip packer.zip
     mv packer /usr/local/bin/packer
