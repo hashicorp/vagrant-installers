bbfe76801f81baad5b8973ff30aa80c7a6a33762
Chris Roberts <croberts@hashicorp.com>
2018-08-28 12:00:46 -0700

    Only use pkg-config generated values. Update library path building.

--- package/appimage/vagrant_wrapper.sh
+++ package/appimage/vagrant_wrapper.sh
@@ -15,9 +15,18 @@ fi
 if [ "${CURL_CA_BUNDLE}" = "" ]; then
     export CURL_CA_BUNDLE="${DIR}/../../etc/ssl/ca-certificates.crt"
 fi
-export LDFLAGS="-L${DIR}/../lib/x86_64-linux-gnu -L${DIR}/../../lib/x86_64-linux-gnu ${LDFLAGS}"
-export CFLAGS="-I${DIR}/../include ${CFLAGS}"
-export CPPFLAGS="-I${DIR}/../include ${CPPFLAGS}"
+
+new_pkg_config_path="${DIR}/usr/lib/x86_64-linux-gnu/pkgconfig"
+
+if [ "${PKG_CONFIG_PATH}" != "" ]; then
+    new_pkg_config_path="${new_pkg_config_path}:${PKG_CONFIG_PATH}"
+fi
+
+if [ -x "$(command -v pkg-config)" ]; then
+    new_pkg_config_path="${new_pkg_config_path}:$(pkg-config --variable pc_path pkg-config)"
+fi
+
+export PKG_CONFIG_PATH="${new_pkg_config_path}"
 export VAGRANT_INSTALLER_ENV="1"
 export VAGRANT_APPIMAGE="1"
 export VAGRANT_INSTALLER_EMBEDDED_DIR="${DIR}/../.."
@@ -29,6 +38,22 @@ else
     export PATH="${DIR}:${PATH}"
 fi
 
-export VAGRANT_APPIMAGE_LD_LIBRARY_PATH=$(ldconfig -N -X -v 2>&1 | grep "^/.*:$" | tr -d ":" | tr "\n" ":")
+new_ld_library_path="${DIR}/lib/x86_64-linux-gnu:${DIR}/usr/lib/x86_64-linux-gnu"
+
+if [ -x "$(command -v ldconfig)" ]; then
+    extra_ld_path=$(ldconfig -N -X -v 2>&1 | grep "^/.*:$" | tr -d ":" | tr "\n" ":")
+else
+    extra_ld_path="/lib:/lib64:/usr/lib:/usr/lib64"
+fi
+
+if [ "${extra_ld_path}" != "" ]; then
+    new_ld_library_path="${new_ld_library_path}:${extra_ld_path}"
+fi
+
+if [ "${LD_LIBRARY_PATH}" != "" ]; then
+    new_ld_library_path="${new_ld_library_path}:${LD_LIBRARY_PATH}"
+fi
+
+export VAGRANT_APPIMAGE_LD_LIBRARY_PATH="${new_ld_library_path}"
 
 "${DIR}/ruby2.4" -- "${DIR}/../gembundle/bin/vagrant" "$@"
