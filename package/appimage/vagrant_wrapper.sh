3bc7fb4d7771f13efdd7408461ee69866247a67d
Chris Roberts <croberts@hashicorp.com>
2018-10-09 13:47:06 -0700

    Update environment variable setup prior to launch

--- package/appimage/vagrant_wrapper.sh
+++ package/appimage/vagrant_wrapper.sh
@@ -4,19 +4,23 @@ SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
+export RUBYLIB=""
+export RUBYOPT=""
+
 export VAGRANT_BIN_DIR="${DIR}"
-export VAGRANT_USR_DIR="${DIR}/../"
-export GEM_HOME="${DIR}/../gembundle"
-export GEM_PATH="${DIR}/../gembundle"
-export RUBYLIB=$( "${DIR}/ruby2.4" -e "puts $:.join(':').gsub('././', ENV['VAGRANT_USR_DIR'])")
+export VAGRANT_USR_DIR="$( cd -P "$( dirname "$VAGRANT_BIN_DIR" )" && pwd )"
+export VAGRANT_ROOT_DIR="$( cd -P "$( dirname "$VAGRANT_USR_DIR" )" && pwd )"
+export GEM_HOME="${VAGRANT_USR_DIR}/gembundle"
+export GEM_PATH="${VAGRANT_USR_DIR}/gembundle"
+export RUBYLIB=$( "${VAGRANT_BIN_DIR}/ruby2.4" -e "puts $:.join(':').gsub('././', ENV['VAGRANT_USR_DIR'])")
 if [ "${SSL_CERT_FILE}" = "" ]; then
-    export SSL_CERT_FILE="${DIR}/../../etc/ssl/ca-certificates.crt"
+    export SSL_CERT_FILE="${VAGRANT_ROOT_DIR}/etc/ssl/ca-certificates.crt"
 fi
 if [ "${CURL_CA_BUNDLE}" = "" ]; then
-    export CURL_CA_BUNDLE="${DIR}/../../etc/ssl/ca-certificates.crt"
+    export CURL_CA_BUNDLE="${VAGRANT_ROOT_DIR}/etc/ssl/ca-certificates.crt"
 fi
 
-new_pkg_config_path="${DIR}/usr/lib/x86_64-linux-gnu/pkgconfig"
+new_pkg_config_path="${VAGRANT_ROOT_DIR}/usr/lib/x86_64-linux-gnu/pkgconfig"
 
 if [ "${PKG_CONFIG_PATH}" != "" ]; then
     new_pkg_config_path="${new_pkg_config_path}:${PKG_CONFIG_PATH}"
@@ -29,7 +33,7 @@ fi
 export PKG_CONFIG_PATH="${new_pkg_config_path}"
 export VAGRANT_INSTALLER_ENV="1"
 export VAGRANT_APPIMAGE="1"
-export VAGRANT_INSTALLER_EMBEDDED_DIR="${DIR}/../.."
+export VAGRANT_INSTALLER_EMBEDDED_DIR="${VAGRANT_ROOT_DIR}"
 export NOKOGIRI_USE_SYSTEM_LIBRARIES="1"
 
 if [ "${VAGRANT_PREFER_SYSTEM_BIN}" != "" -a "${VAGRANT_PREFER_SYSTEM_BIN}" != "0" ]; then
@@ -38,7 +42,7 @@ else
     export PATH="${DIR}:${PATH}"
 fi
 
-new_ld_library_path="${DIR}/lib/x86_64-linux-gnu:${DIR}/usr/lib/x86_64-linux-gnu"
+new_ld_library_path="${VAGRANT_ROOT_DIR}/lib/x86_64-linux-gnu:${VAGRANT_ROOT_DIR}/usr/lib/x86_64-linux-gnu"
 
 if [ -x "$(command -v ldconfig)" ]; then
     extra_ld_path=$(ldconfig -N -X -v 2>&1 | grep "^/.*:$" | tr -d ":" | tr "\n" ":")
@@ -56,4 +60,4 @@ fi
 
 export VAGRANT_APPIMAGE_LD_LIBRARY_PATH="${new_ld_library_path}"
 
-"${DIR}/ruby2.4" -- "${DIR}/../gembundle/bin/vagrant" "$@"
+"${VAGRANT_BIN_DIR}/ruby2.4" -- "${VAGRANT_USR_DIR}/gembundle/bin/vagrant" "$@"
