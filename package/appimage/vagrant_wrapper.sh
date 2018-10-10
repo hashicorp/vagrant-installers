72b925b083d06dc3bc7ed664480df94a7cac6715
Chris Roberts <croberts@hashicorp.com>
2018-10-10 16:32:49 -0700

    Update environment variable generation within appimage
    Clean up environment variable generation and path usage. Fix up
    customized RUBYLIB to contain within the appimage. Scrub out
    python provided env vars since we do not want to isolate any
    python within the appimage.
    
    Fixes hashicorp/vagrant#10281

--- package/appimage/vagrant_wrapper.sh
+++ package/appimage/vagrant_wrapper.sh
@@ -12,7 +12,7 @@ export VAGRANT_USR_DIR="$( cd -P "$( dirname "$VAGRANT_BIN_DIR" )" && pwd )"
 export VAGRANT_ROOT_DIR="$( cd -P "$( dirname "$VAGRANT_USR_DIR" )" && pwd )"
 export GEM_HOME="${VAGRANT_USR_DIR}/gembundle"
 export GEM_PATH="${VAGRANT_USR_DIR}/gembundle"
-export RUBYLIB=$( "${VAGRANT_BIN_DIR}/ruby2.4" -e "puts $:.join(':').gsub('././', ENV['VAGRANT_USR_DIR'])")
+export RUBYLIB="$( "${VAGRANT_BIN_DIR}/ruby2.4" -e "puts $:.map{|x| ENV['VAGRANT_ROOT_DIR'] + x}.join(':')" )"
 if [ "${SSL_CERT_FILE}" = "" ]; then
     export SSL_CERT_FILE="${VAGRANT_ROOT_DIR}/etc/ssl/ca-certificates.crt"
 fi
@@ -36,13 +36,17 @@ export VAGRANT_APPIMAGE="1"
 export VAGRANT_INSTALLER_EMBEDDED_DIR="${VAGRANT_ROOT_DIR}"
 export NOKOGIRI_USE_SYSTEM_LIBRARIES="1"
 
+# Set these for easier debugging so they show up in the logs
+export VAGRANT_PKG_CONFIG_PATH="${PKG_CONFIG_PATH}"
+export VAGRANT_RUBYLIB="${RUBYLIB}"
+
 if [ "${VAGRANT_PREFER_SYSTEM_BIN}" != "" -a "${VAGRANT_PREFER_SYSTEM_BIN}" != "0" ]; then
     export PATH="${PATH}:${DIR}"
 else
     export PATH="${DIR}:${PATH}"
 fi
 
-new_ld_library_path="${VAGRANT_ROOT_DIR}/lib/x86_64-linux-gnu:${VAGRANT_ROOT_DIR}/usr/lib/x86_64-linux-gnu"
+new_ld_library_path="${LD_LIBRARY_PATH}"
 
 if [ -x "$(command -v ldconfig)" ]; then
     extra_ld_path=$(ldconfig -N -X -v 2>&1 | grep "^/.*:$" | tr -d ":" | tr "\n" ":")
@@ -54,10 +58,10 @@ if [ "${extra_ld_path}" != "" ]; then
     new_ld_library_path="${new_ld_library_path}:${extra_ld_path}"
 fi
 
-if [ "${LD_LIBRARY_PATH}" != "" ]; then
-    new_ld_library_path="${new_ld_library_path}:${LD_LIBRARY_PATH}"
-fi
-
 export VAGRANT_APPIMAGE_LD_LIBRARY_PATH="${new_ld_library_path}"
+
+# Python variables will be set but we don't want them
+unset PYTHONHOME
+unset PYTHONPATH
 
 "${VAGRANT_BIN_DIR}/ruby2.4" -- "${VAGRANT_USR_DIR}/gembundle/bin/vagrant" "$@"
