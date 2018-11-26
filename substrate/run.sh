f708016d687e0a0b8c8a6c621a9bb366ade9de6c
Anton Antonov <anton.synd.antonov@gmail.com>
2018-11-26 18:29:27 +0200

    Update Ruby to 2.5.3

--- substrate/run.sh
+++ substrate/run.sh
@@ -16,7 +16,7 @@ libxslt_version="1.1.32"
 libyaml_version="0.1.7"
 openssl_version="1.1.0g"
 readline_version="7.0"
-ruby_version="2.4.4"
+ruby_version="2.5.3"
 xz_version="5.2.3"
 zlib_version="1.2.11"
 
