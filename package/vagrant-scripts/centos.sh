56d918c8617f36efc4090598be1b285ea02a38f7
Chris Roberts <croberts@hashicorp.com>
2019-06-11 21:27:45 -0700

    Use updated gcc to build properly with libxcrypt updates

--- package/vagrant-scripts/centos.sh
+++ package/vagrant-scripts/centos.sh
@@ -2,14 +2,18 @@
 
 yum install -y nc zip unzip chrpath
 
-sudo yum install -y centos-release-scl
-sudo yum install -y  devtoolset-8-toolchain
-source /opt/rh/devtoolset-8/enable
-
 # if the proxy is around, use it
 nc -z -w3 192.168.1.1 8123 && export http_proxy="http://192.168.1.1:8123"
 mkdir -p /vagrant/substrate-assets
 chmod 755 /vagrant/package/package.sh
+
+hostos=$(uname -a)
+
+if [[ "${hostos}" = *"x86_64"* ]]; then
+    sudo yum install -y centos-release-scl
+    sudo yum install -y devtoolset-8-toolchain
+    source /opt/rh/devtoolset-8/enable
+fi
 
 set -e
 
