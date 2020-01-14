016e536e030da8f7ffffc8ad2f73aa80ce708e7e
Chris Roberts <croberts@hashicorp.com>
2020-01-14 08:22:32 -0800

    Include more debug information

--- .ci/sync.sh
+++ .ci/sync.sh
@@ -16,9 +16,18 @@ else
     remote_repository="hashicorp/vagrant-installers"
 fi
 
+echo "Adding mirror repository `${remote_repository}`..."
 wrap_stream git remote add mirror "https://${HASHIBOT_USERNAME}:${HASHIBOT_TOKEN}@github.com/${remote_repository}" \
-     "Failed to add mirror repository (${remote_repository}) for sync"
+            "Failed to add mirror repository (${remote_repository}) for sync"
+echo "Currently configured remotes:"
+git remote -v
+
+echo "Updating configured remotes..."
 wrap_stream git remote update \
-     "Failed to update mirror repository (${remote_repository}) for sync"
+            "Failed to update mirror repository (${remote_repository}) for sync"
+echo "Currently loaded refs:"
+git branch -a
+
+echo "Pushing master to mirror..."
 wrap_stream git push mirror master \
-     "Failed to sync mirror repository (${remote_repository})"
+            "Failed to sync mirror repository (${remote_repository})"
