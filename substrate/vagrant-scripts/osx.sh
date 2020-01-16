e5f7c61a864ef689c195f4711bcfca5781f6d910
Chris Roberts <croberts@hashicorp.com>
2020-01-16 14:56:21 -0800

    Remove cruft from setup script

--- substrate/vagrant-scripts/osx.sh
+++ substrate/vagrant-scripts/osx.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-# if the proxy is around, use it
-nc -z -w3 192.168.1.1 8123 && export http_proxy="http://192.168.1.1:8123"
 
 OUTPUT_DIR="${VAGRANT_SUBSTRATE_OUTPUT_DIR:-substrate-assets}"
 mkdir -p /vagrant/${OUTPUT_DIR}
 chmod 755 /vagrant/substrate/run.sh
 
-TRAVIS=1 su vagrant -l -c 'brew update'
-TRAVIS=1 su vagrant -l -c 'brew install wget'
+su vagrant -l -c 'brew update'
+su vagrant -l -c 'brew install wget'
 
 # grab new cacert
 curl -o cacert.pem https://curl.haxx.se/ca/cacert.pem
