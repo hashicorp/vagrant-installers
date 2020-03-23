9598c2c407c92e5884a1c027b756497914c285fc
Chris Roberts <croberts@hashicorp.com>
2020-03-23 10:54:00 -0700

    Update go version installed on builder guest

--- packer/vagrant/scripts/windows/go.bat
+++ packer/vagrant/scripts/windows/go.bat
@@ -1,5 +1,5 @@
 if not exist "C:\Windows\Temp\go.msi" (
-powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12; (New-Object System.Net.WebClient).DownloadFile('https://storage.googleapis.com/golang/go1.10.1.windows-amd64.msi', 'C:\Windows\Temp\go.msi')" <NUL
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12; (New-Object System.Net.WebClient).DownloadFile('https://dl.google.com/go/go1.14.1.windows-amd64.msi', 'C:\Windows\Temp\go.msi')" <NUL
 )
 
 set SEE_MASK_NOZONECHECKS=1
