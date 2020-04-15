297f1eaa07072a6faa1d903c25d2f7b8f824b8ff
Chris Roberts <croberts@hashicorp.com>
2020-04-14 17:15:28 -0700

    Bump Ruby to 2.6.6

--- substrate/run.sh
+++ substrate/run.sh
@@ -16,7 +16,7 @@ libxslt_version="1.1.32"
 libyaml_version="0.1.7"
 openssl_version="1.1.1f"
 readline_version="7.0"
-ruby_version="2.6.5"
+ruby_version="2.6.6"
 xz_version="5.2.4"
 zlib_version="1.2.11"
 
