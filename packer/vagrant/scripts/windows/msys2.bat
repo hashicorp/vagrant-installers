feb7d15dc0d1c4b184f6c4e34aa3e7963a4417a1
Chris Roberts <croberts@hashicorp.com>
2020-06-12 09:26:17 -0700

    Update msys2 installer url

--- packer/vagrant/scripts/windows/msys2.bat
+++ packer/vagrant/scripts/windows/msys2.bat
@@ -1,5 +1,5 @@
 if not exist "C:\Windows\Temp\msys2.exe" (
-powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12; (New-Object System.Net.WebClient).DownloadFile('http://repo.msys2.org/distrib/x86_64/msys2-x86_64-20161025.exe', 'C:\Windows\Temp\msys2.exe')" <NUL
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/msys2/msys2-installer/releases/download/2020-06-02/msys2-x86_64-20200602.exe', 'C:\Windows\Temp\msys2.exe')" <NUL
 )
 
 C:\Windows\Temp\msys2.exe --script A:\msys2-install.qs
