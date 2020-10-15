dcb3ce1b7dfed582615f42a4a6226002bc0e6f19
Chris Roberts <croberts@hashicorp.com>
2020-10-15 13:04:57 -0700

    Update testing repository name

--- .ci/build-packages.sh
+++ .ci/build-packages.sh
@@ -337,8 +337,8 @@ else
 
     slack -m "New Vagrant development installers available:\n> https://github.com/${repository}/releases/${prerelease_version}"
 
-    echo "Dispatching blackbox"
-    curl -X POST "https://api.github.com/repos/hashicorp/vagrant-blackbox/dispatches" \
+    echo "Dispatching vagrant-acceptance"
+    curl -X POST "https://api.github.com/repos/hashicorp/vagrant-acceptance/dispatches" \
     -H 'Accept: application/vnd.github.everest-v3+json' \
     -u $HASHIBOT_USERNAME:$HASHIBOT_TOKEN \
     --data '{"event_type": "prerelease", "client_payload": { "prerelease_version": "'"${prerelease_version}"'"}}'
