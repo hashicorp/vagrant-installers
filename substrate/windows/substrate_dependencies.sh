8e54396149b8f621f9da599dc4c98796a7e06f5e
sophia <scastellarin95@gmail.com>
2020-11-17 15:38:36 -0600

    Update windows builds

--- substrate/windows/substrate_dependencies.sh
+++ substrate/windows/substrate_dependencies.sh
@@ -6,5 +6,10 @@ rm -rf /etc/pacman.d/gnupg
 
 pacman-key --init
 pacman-key --populate msys2
+
+# https://github.com/msys2/MSYS2-packages/issues/2225
+curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz
+pacman -U msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz --noconfirm
+
 pacman-key --refresh-keys
-pacman -Syu --noconfirm
+pacman -Syu --noconfirm --disable-download-timeout
