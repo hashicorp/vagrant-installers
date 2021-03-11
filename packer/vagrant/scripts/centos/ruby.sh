e05f57c31585a795f5ec4ae72b195133a95dc923
sophia <scastellarin95@gmail.com>
2021-03-11 10:34:09 -0600

    Centos 7 i386 box

--- packer/vagrant/scripts/centos/ruby.sh
+++ packer/vagrant/scripts/centos/ruby.sh
@@ -5,11 +5,5 @@ pushd /tmp
 # system update
 yum -y install libxslt-devel libyaml-devel libxml2-devel gdbm-devel libffi-devel zlib-devel openssl-devel libyaml-devel readline-devel curl-devel openssl-devel pcre-devel git
 
-version=2.4.3
-cd /usr/local/src
-wget https://cache.ruby-lang.org/pub/ruby/2.4/ruby-$version.tar.gz
-tar zxvf ruby-$version.tar.gz
-cd ruby-$version
-./configure --prefix=/usr
-make
-make install
+yum -y install ruby
+
