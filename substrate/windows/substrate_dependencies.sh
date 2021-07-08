30e87d30089ebd2197a753139548e06210f94c9e
Chris Roberts <croberts@hashicorp.com>
2021-07-08 11:28:42 -0700

    Always specify keyserver

--- substrate/windows/substrate_dependencies.sh
+++ substrate/windows/substrate_dependencies.sh
@@ -5,6 +5,6 @@ echo "Updating host packages before building substrate"
 rm -rf /etc/pacman.d/gnupg
 
 pacman-key --init --keyserver https://keyserver.ubuntu.com/
-pacman-key --populate msys2
-pacman-key --refresh-keys
+pacman-key --populate msys2 --keyserver https://keyserver.ubuntu.com/
+pacman-key --refresh-keys --keyserver https://keyserver.ubuntu.com/
 pacman -Syu --noconfirm
