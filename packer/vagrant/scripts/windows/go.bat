89eff7553e83589f5fc0158712dfdd739b55e0ff
Chris Roberts <croberts@hashicorp.com>
2022-09-23 15:09:46 -0700

    Escape quotes within command

--- packer/vagrant/scripts/windows/go.bat
+++ packer/vagrant/scripts/windows/go.bat
@@ -1,4 +1,4 @@
-powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12; (New-Object System.Net.WebClient).DownloadFile('https://dl.google.com/go/go${env:GO_INSTALL_VERSION}.windows-amd64.msi', 'C:\Windows\Temp\go.msi')" <NUL
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12; (New-Object System.Net.WebClient).DownloadFile(\"https://dl.google.com/go/go${env:GO_INSTALL_VERSION}.windows-amd64.msi\", 'C:\Windows\Temp\go.msi')" <NUL
 
 set SEE_MASK_NOZONECHECKS=1
 cmd /c msiexec /qn /i C:\Windows\Temp\go.msi
