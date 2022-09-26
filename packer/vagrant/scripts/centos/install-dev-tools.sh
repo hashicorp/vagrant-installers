664b6c88b7c5d226eecd567010d5c134dbb67149
Chris Roberts <croberts@hashicorp.com>
2022-09-26 12:33:42 -0700

    Include python headers in package list

--- packer/vagrant/scripts/centos/install-dev-tools.sh
+++ packer/vagrant/scripts/centos/install-dev-tools.sh
@@ -2,4 +2,4 @@
 
 yum groupinstall -yq "development tools"
 yum install -yq perl make kernel-headers kernel-devel wget curl  \
-  rsync openssl-devel readline-devel zlib-devel net-tools nfs-utils
+  rsync openssl-devel readline-devel zlib-devel net-tools nfs-utils python-devel
