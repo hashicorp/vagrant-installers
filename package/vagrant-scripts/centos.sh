d3133f13a07d34bb852c26a07cae898ae87a26aa
Chris Roberts <croberts@hashicorp.com>
2022-09-26 15:42:49 -0700

    Fix arguments in package call

--- package/vagrant-scripts/centos.sh
+++ package/vagrant-scripts/centos.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
 
-# Create our local destination for the assets
-mkdir -p /vagrant/substrate-assets
 # Ensure our package script is executable
 chmod 755 /vagrant/package/package.sh
 
 set -e
 
-/vagrant/package/package.sh "/vagrant/substrate-assets/substrate_centos_$(uname -m).zip main"
+/vagrant/package/package.sh "/vagrant/substrate-assets/substrate_centos_$(uname -m).zip" "main"
 pkg_dir=${VAGRANT_PACKAGE_OUTPUT_DIR:-"pkg"}
 mkdir -p "/vagrant/${pkg_dir}"
 cp ./*.rpm "/vagrant/${pkg_dir}/"
