8a34ea4f4a0d7edd38f75649536f357b47c9cb6d
Chris Roberts <croberts@hashicorp.com>
2022-11-07 14:48:10 -0800

    Extract required artifact downloads

--- .ci/build-packages.sh
+++ .ci/build-packages.sh
@@ -45,25 +45,6 @@ if [ -z "${packages_needed}" ]; then
     exit
 fi
 
-# Download the Vagrant RubyGem and vagrant-go
-# binaries. These come from different locations
-# depending on how the job was invoked. If the
-# VAGRANT_REF environment variable is set, it was
-# invoked from a non-release job and the artifacts
-# will be retrieved from a draft release on the
-# Vagrant repository. If a tag is set, then the
-# artifacts will be in a proper release within the
-# builders repository. Otherwise, we grab the
-# artifacts from the "main" draft release in the
-# Vagrant repository
-if [ -n "${VAGRANT_REF}" ]; then
-    github_draft_release_assets "hashicorp" "vagrant" "${VAGRANT_REF}"
-elif [ -n "${tag}" ]; then
-    github_release_assets "hashicorp" "vagrant" "${tag}"
-else
-    github_draft_release_assets "hashicorp" "vagrant" "main"
-fi
-
 # Extract out Vagrant version information from gem
 vagrant_version="$(gem specification vagrant-*.gem version)" ||
     fail "Failed to ready version from Vagrant RubyGem"
