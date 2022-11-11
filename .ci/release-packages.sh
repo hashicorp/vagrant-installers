df83d31b72ecc9b473c13491e1ee141f98617eef
Chris Roberts <croberts@hashicorp.com>
2022-11-11 11:08:26 -0800

    consistent dev build

--- .ci/release-packages.sh
+++ .ci/release-packages.sh
@@ -8,11 +8,6 @@ root="$( cd -P "$( dirname "$csource" )/../" && pwd )"
 
 pushd "${root}"
 
-if [ -z "${release}" ]; then
-    echo "Not creating release: this is a dev build"
-    exit
-fi
-
 mkdir -p ./pkg
 
 if [ -n "${PACKAGES_IDENTIFIER}" ]; then
@@ -21,6 +16,29 @@ if [ -n "${PACKAGES_IDENTIFIER}" ]; then
     popd
 else
     fail "No identifier defined for packages"
+fi
+
+# If this is not a release build, push the prerelease to GitHub
+if [ -z "${release}" ]; then
+    if [ -n "${tag}" ]; then
+        prerelease_version="${tag}"
+    else
+        prerelease_version="v${vagrant_version}+${ident_ref}"
+    fi
+
+    # Make the prerelease version available for other jobs
+    printf "prerelease-version=%s\n" "${prerelease_version}" >> $GITHUB_OUTPUT
+
+    echo "Generating GitHub pre-release packages for Vagrant version ${prerelease_version}... "
+    # NOTE: We always want to store builds into the vagrant-installers repository since they should
+    # be publicly accessible
+    export repo_name="vagrant-installers"
+    export GITHUB_TOKEN="${HASHIBOT_TOKEN}"
+    prerelease "${prerelease_version}" pkg/
+
+    slack -m "New Vagrant development installers available:\n> https://github.com/${repo_owner}/${repo_name}/releases/${prerelease_version}"
+
+    exit
 fi
 
 # Otherwise, this is a proper release, so invoke the hashicorp release
