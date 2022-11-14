28a3a8a1e1fdbaa8114c726842b7aad770cb6c33
Chris Roberts <croberts@hashicorp.com>
2022-11-14 12:35:42 -0800

    update installer version

--- packer/vagrant/scripts/windows/msys2-install.bat
+++ packer/vagrant/scripts/windows/msys2-install.bat
@@ -1,5 +1,5 @@
 
-powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12; (New-Object System.Net.WebClient).DownloadFile('https://vagrant-public-cache.s3.amazonaws.com/msys2-x86_64-20210604.exe', 'C:\Windows\Temp\msys2.exe')" <NUL
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12; (New-Object System.Net.WebClient).DownloadFile('https://vagrant-public-cache.s3.amazonaws.com/msys2-x86_64-20221028.exe', 'C:\Windows\Temp\msys2.exe')" <NUL
 
 
 cmd /c "C:\Windows\Temp\msys2.exe -d --platform minimal --script A:\msys2-install.qs"
