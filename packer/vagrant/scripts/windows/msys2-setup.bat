456e40a114a2896de1516f20938313cb9fa26e02
sophia <scastellarin95@gmail.com>
2023-02-08 09:44:19 -0800

    Install openssl 1.1.1

--- packer/vagrant/scripts/windows/msys2-setup.bat
+++ packer/vagrant/scripts/windows/msys2-setup.bat
@@ -1,1 +1,7 @@
 cmd /c "C:\msys64\usr\bin\bash.exe --login -c "pacman -S --noconfirm zip unzip base-devel""
+
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12; (New-Object System.Net.WebClient).DownloadFile('https://vagrant-public-cache.s3.amazonaws.com/mingw-w64-x86_64-openssl-1.1.1.s-1-any.pkg.tar.zst', 'C:\Windows\Temp\openssl-64.tar.zst')" <NUL
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12; (New-Object System.Net.WebClient).DownloadFile('https://vagrant-public-cache.s3.amazonaws.com/mingw-w64-i686-openssl-1.1.1.s-1-any.pkg.tar.zst', 'C:\Windows\Temp\openssl-32.tar.zst')" <NUL
+
+cmd /c "C:\msys64\usr\bin\bash.exe --login -c "pacman -U --noconfirm C:\\Windows\\Temp\\openssl-64.tar.zst""
+cmd /c "C:\msys64\usr\bin\bash.exe --login -c "pacman -U --noconfirm C:\\Windows\\Temp\\openssl-32.tar.zst""
