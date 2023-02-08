26d48573dd93d357434b8060946060d734a5e5b6
sophia <scastellarin95@gmail.com>
2023-02-08 11:05:07 -0800

    Fix backslashes

--- packer/vagrant/scripts/windows/msys2-setup.bat
+++ packer/vagrant/scripts/windows/msys2-setup.bat
@@ -3,5 +3,5 @@ cmd /c "C:\msys64\usr\bin\bash.exe --login -c "pacman -S --noconfirm zip unzip b
 powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12; (New-Object System.Net.WebClient).DownloadFile('https://vagrant-public-cache.s3.amazonaws.com/mingw-w64-x86_64-openssl-1.1.1.s-1-any.pkg.tar.zst', 'C:\Windows\Temp\openssl-64.tar.zst')" <NUL
 powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12; (New-Object System.Net.WebClient).DownloadFile('https://vagrant-public-cache.s3.amazonaws.com/mingw-w64-i686-openssl-1.1.1.s-1-any.pkg.tar.zst', 'C:\Windows\Temp\openssl-32.tar.zst')" <NUL
 
-cmd /c "C:\msys64\usr\bin\bash.exe --login -c "pacman -U --noconfirm C:\\Windows\\Temp\\openssl-64.tar.zst""
-cmd /c "C:\msys64\usr\bin\bash.exe --login -c "pacman -U --noconfirm C:\\Windows\\Temp\\openssl-32.tar.zst""
+cmd /c "C:\msys64\usr\bin\bash.exe --login -c "pacman -U --noconfirm C:\\/Windows\\/Temp\\/openssl-64.tar.zst""
+cmd /c "C:\msys64\usr\bin\bash.exe --login -c "pacman -U --noconfirm C:\\/Windows\\/Temp\\/openssl-32.tar.zst""
