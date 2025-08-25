188a6bb130ffa52d4a3be786a84e05e1d162bed8
David Zülke <dzuelke@gmail.com>
2025-08-25 12:32:08 +0200

    Update cacert to 2025-08-12

--- substrate/deps.sh
+++ substrate/deps.sh
@@ -93,8 +93,8 @@ libxcrypt_version="4.4.38"
 libxcrypt_shasum="80304b9c306ea799327f01d9a7549bdb28317789182631f1b54f4511b4206dd6"
 
 # cacert (.pem) - https://curl.se/docs/sslcerts.html
-cacert_version="2025-02-25"
-cacert_shasum="50a6277ec69113f00c5fd45f09e8b97a4b3e32daa35d3a95ab30137a55386cef"
+cacert_version="2025-08-12"
+cacert_shasum="64dfd5b1026700e0a0a324964749da9adc69ae5e51e899bf16ff47d6fd0e9a5e"
 
 # Only used for macOS
 # NOTE: The 10.12 SDK was the earliest version of the
